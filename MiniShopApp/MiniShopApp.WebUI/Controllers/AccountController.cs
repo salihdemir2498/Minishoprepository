78cf950cbe5e35f9f57b850b5c9f7575247ba76f
salihdemir2498 <salihhdemir1727@gmail.com>
2022-05-27 13:49:09 +0300

    27 mart update

---
+++ MiniShopApp/MiniShopApp.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniShopApp.WebUI.Identity;
+using MiniShopApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string ReturnUrl=null) 
+        {
+            return View(
+                   new LoginModel()
+                   {
+                       ReturnUrl = ReturnUrl
+                   }
+                );
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            //Burada login işlemlerini gerçekleştireceğiz.
+            return View(); 
+        }
+
+        public IActionResult Register() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = new User()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)//Create işlemi başarılıysa
+            {
+                //Mail onay işlemleri
+                //TOKEN işlemleri
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                
+                var url = Url.Action("ConfirmEmail", "Account", new
+                {
+                    userId = user.Id,
+                    token = code
+                });
+                //mail gönderme işlemleri
+
+
+            }
+            return View();
+        }
+    }
+}
---
+++ MiniShopApp/MiniShopApp.WebUI/EmailServices/SmtpEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.WebUI.EmailServices
+{
+    public class SmtpEmailSender : IEmailSender
+    {
+        //
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
